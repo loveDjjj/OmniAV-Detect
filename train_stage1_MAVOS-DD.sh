3c67508e0525889ab3c3947cd19753bdc01985fc
loveDjjj <1545007810@qq.com>
2026-05-02 01:45:52 +0800

    add train_all.sh

--- train_stage1_MAVOS-DD.sh
+++ train_stage1_MAVOS-DD.sh
@@ -25,7 +25,7 @@ NPROC_PER_NODE=2 CUDA_VISIBLE_DEVICES=0,1 swift sft \
   --dataloader_num_workers 8 \
   --learning_rate 1e-5 \
   --logging_steps 10 \
-  --save_steps 500 \
+  --save_steps 100 \
   --save_total_limit 2 \
   --gradient_checkpointing true \
-  --output_dir /data/OneDay/OmniAV-Detect/outputs/stage1_qwen2_5_omni_mavosdd_binary_lora_audio_in_video_fast
+  --output_dir /data/OneDay/OmniAV-Detect/outputs/stage1_qwen2_5_omni_mavosdd_binary_audio_in_video
